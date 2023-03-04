e33815fd7a8ae139a0b20669dced91084b592621
linyicheng <106894066+yicheng0212@users.noreply.github.com>
2023-03-04 11:22:20 +0800

    Add files via upload

--- tickets.php
+++ tickets.php
@@ -1,15 +1,10 @@
 <?php
  session_start();
     if($_SESSION['veri']==$_POST['veri']){
-        $dsn="mysql:host=localhost;charset=utf8;dbname=db25";
-        $pdo=new PDO($dsn,'root','');
-        $sql="INSERT INTO `tickets` (`first_name`,`last_name`,`phone`,`password`)
-                   values('{$_POST['first_name']}',
-                          '{$_POST['last_name']}',
-                          '{$_POST['phone']}',
-                          '{$_POST['password']}')";
+        $dsn="mysql:host=localhost;charset=utf8;dbname=db01";
+        $pdo=new PDO($dsn,'admin','1234');
+        $sql="INSERT INTO `tickets` (`first_name`,`last_name`,`phone`,`password`) values('{$_POST['first_name']}','{$_POST['last_name']}','{$_POST['phone']}','{$_POST['password']}')";
         $pdo->exec($sql);
-    
         header("location:home.html"); 
     }else{
 ?>
